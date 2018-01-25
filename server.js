6fbe08bdb841dcba732a739b54318cb2431d6293
Brandon Fowler <bmfowler4@gmail.com>
2018-01-24 17:03:12 -1000

    server updated to handle get, post, put and delete requests

--- server.js
+++ server.js
@@ -31,12 +31,12 @@ server.listen(8080, () => {
 });
 
 function doRequest(data, client) {
-  let requestLine = data.split(`\n`)[0].split(` `);
-  let requestType = requestLine[0];
-  let path = requestLine[1];
-  let httpVersion = requestLine[2];
-  let date = new Date().toUTCString();
-  let responseHeaders = `Server: Brandon\nDate: ${date}`;
+  const requestLine = data.split(`\n`)[0].split(` `);
+  const requestType = requestLine[0];
+  const path = requestLine[1];
+  const httpVersion = requestLine[2];
+  const date = new Date().toUTCString();
+  const responseHeaders = `Server: Brandon\nDate: ${date}`;
   let statusLine;
   let body;
   if (requestType === `GET`) {
@@ -49,8 +49,18 @@ function doRequest(data, client) {
     }
     let message = `${statusLine}\n${responseHeaders}\n\n${body}`;
     client.write(message);
-  } else if (requestType === `PUT`) {
-    console.log(data);
+  } else if (requestType === `PUT` || requestType === `POST`) {
+    const created = !files.hasOwnProperty(path);
+    body = data.split(`\r\n\r\n`)[1];
+    statusLine = created ? `HTTP/1.1 201 Created` : `HTTP/1.1 200 OK`;
+    files[path] = body;
+    client.write(`${statusLine}\n${responseHeaders}\n\n`);
   } else {
+    if (files.hasOwnProperty(path)) {
+      delete files[path];
+      client.write(`HTTP/1.1 200 OK\n${responseHeaders}\n\n`);
+    } else {
+      client.write(`HTTP/1.1 404 NOT FOUND\n\n`);
+    }
   }
 }
